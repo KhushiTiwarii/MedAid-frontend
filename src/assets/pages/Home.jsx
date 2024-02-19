ae7744da78c9feb5d090420d1b64c2b2d0210fb4
khushi123go <tiwarikhushi907@gmail.com>
2024-02-20 00:42:35 +0530

    3.About

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import heroImg01 from '../../assets/images/hero-img01.jpg'
 import heroImg02 from '../../assets/images/hero-img02.jpg'
 import heroImg03 from '../../assets/images/hero-img03.jpg'
+import icon01 from '../../assets/images/icon01.png'
+import icon02 from '../../assets/images/icon02.png'
+import icon03 from '../../assets/images/icon03.png'
+import { Link } from 'react-router-dom'
+import {BsArrowRight} from 'react-icons/bs'
+import About from '../../components/About/About'
 
 const Home = () => {
   return (
@@ -36,7 +42,7 @@ const Home = () => {
               </div>
             </div>
            </div>
-          {/*  */}
+          {/* herocontent end */}
 
           <div className="flex gap-[30px] justify-end">
             <div>
@@ -51,6 +57,63 @@ const Home = () => {
         </div>
       </div>
     </section>
+    {/* hero section end*/}
+
+    <section>
+      <div className="container">
+        <div className="lg:w-[470px] mx-auto">
+          <h2 className="heading text-center">Providing the best mest medical services</h2>
+          <p className='text_para text-center'>
+            World-class care for everyone. Our health System offers unmatched,
+            expert health care.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] my-[30px] lg:mt-[55px]">
+
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center "><img src={icon01} alt="" /></div>
+
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Find a Doctor</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.</p>
+              <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center "><img src={icon02} alt="" /></div>
+
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Find a Location</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.</p>
+              <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center "><img src={icon03} alt="" /></div>
+
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Book Appointment</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.</p>
+              <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    {/* about section */}
+    <About/>
+    {/* about section end */}
     </>
   )
 }
